97a5e6df4d185509a3c2d961a0b67adf62dfaedf
SUDHARSAN <kkqqq77288@gmail.com>
2026-02-06 15:35:57 +0530

    /

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sonachala-booking-engine-admin/', // Changed to '/' for Firebase hosting
+  base: '/', // Changed to '/' for Firebase hosting
 })
